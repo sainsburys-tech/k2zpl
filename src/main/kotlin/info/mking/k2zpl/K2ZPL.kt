af06544fa0a11e7c10c0773f48c32d25637e9455
Matt King <matt@mking.info>
2024-07-11 10:49:41 +0100

    Rename main entry point function to `k2zpl`.

--- src/main/kotlin/info/mking/k2zpl/K2ZPL.kt
+++ src/main/kotlin/info/mking/k2zpl/K2ZPL.kt
@@ -3,13 +3,5 @@
 package info.mking.k2zpl
 
 import info.mking.k2zpl.builder.ZplBuilder
-import info.mking.k2zpl.builder.endFormat
-import info.mking.k2zpl.builder.startFormat
 
-fun zpl(init: ZplBuilder.() -> Unit) = ZplBuilder().apply(init).build()
-
-fun label(init: ZplBuilder.() -> Unit) = ZplBuilder().apply {
-    startFormat()
-    init()
-    endFormat()
-}.build()
+fun k2zpl(init: ZplBuilder.() -> Unit) = ZplBuilder().apply(init).build()
