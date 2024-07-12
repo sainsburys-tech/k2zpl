fd73da61ba6f3c6dcedadea593b08dd80e3074b9
Matt King <matt@mking.info>
2024-07-12 16:53:45 +0100

    Remove unused code

--- src/test/kotlin/info/mking/k2zpl/Extensions.kt
+++ src/test/kotlin/info/mking/k2zpl/Extensions.kt
@@ -3,11 +3,3 @@ package info.mking.k2zpl
 import info.mking.k2zpl.command.ZplCommand
 
 fun ZplCommand.testBuildString() = buildString { build(this) }
-
-fun <S, T> List<T>.repeated(times: Int, block: (T) -> S) = flatMap { item ->
-    buildList {
-        repeat(times) {
-            add(block.invoke(item))
-        }
-    }
-}
