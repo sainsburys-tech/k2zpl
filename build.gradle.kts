6d726969bfeceadce23a97f5f4d13a78ce32dba8
matt.king2 <matt.king2@sainsburys.co.uk>
2024-10-31 07:36:18 +0000

    Bump to 0.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.sainsburys"
-version = "0.2.5"
+version = "0.3.0"
 
 repositories {
     mavenCentral()
@@ -36,7 +36,7 @@ publishing {
     }
 }
 mavenPublishing {
-    coordinates("com.sainsburys", "k2zpl", "0.2.5")
+    coordinates("com.sainsburys", "k2zpl", "0.3.0")
     pom {
         name.set("k2zpl")
         description.set("Kotlin DSL for ZPL (Zebra Programming Language)")
